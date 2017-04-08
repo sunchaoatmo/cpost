7cbd76453483bac56b0635c4ea4e7d965e4952cf
Chao Sun <sunchao@umd.edu>
2017-04-08 09:19:18 -0400

    move the default z_level into the argument module

--- argument.py
+++ argument.py
@@ -11,6 +11,9 @@ parser.add_argument("-exe",help="location to the post.exe")
 parser.add_argument("-n",default=getcwd().split("/")[-1],help="name of the case")
 parser.add_argument("-mpi", help="MPI flag, determine whether run the script in MPI mode", action="store_true")
 parser.add_argument("--sjob",help='create and submit jobs',action="store_true")
+z_levs=[1000.0, 975.0, 950.0, 925.0, 900.0, 875.0, 850.0, 825.0, 800.0, 775.0, 750.0, 700.0, 650.0, 600.0, 550.0, 500.0, 450.0, 400.0, 350.0, 300.0, 250.0, 225.0, 200.0, 175.0, 150.0, 125.0, 100.0, 70.0]
+z_levs=[x*100 for x in z_levs]
+parser.add_argument('--z_levs', '-z', nargs='+',help='level to interpolate',default=z_levs, type=int)
 
 args = parser.parse_args() 
 if args.v:
