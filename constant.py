03632e688d067b749fa9dd3f4b17c6a44caa1a94
Chao Sun <sunchao@umd.edu>
2017-04-08 09:17:18 -0400

    add the ability to write different dimesion data fix bugs

--- constant.py
+++ constant.py
@@ -1,3 +1,4 @@
+import numpy as np
 seasonList=[ [12,  2], [3, 5], [6,  8], [9,  11]]
 monthlyList=[ [12, 1], [1, 2],[2,3],[3,4],[4,5],[5,6],[6, 7],[7, 8],[8,9], [9, 10],[10, 11],[11,12]]
 wrfout_data_fmt="%Y-%m-%d_%H:%M:%S"
@@ -7,3 +8,19 @@ computemode=2
 outputdim=3
 dry_lim=1
 pct=0.95
+G = 9.81
+Rd = 287.04
+Rv = 461.6
+Rm = .608 
+Cp = 1004.
+Cp = 7.*Rd/2.
+Cv = Cp-Rd
+CPMD = 0.887
+RCP = Rd/Cp
+p0 = 100000.
+# be awared the z_levs should be sorted from large to small
+z_levs=[1000.0, 975.0, 950.0, 925.0, 900.0, 875.0, 850.0, 825.0, 800.0, 775.0, 750.0, 700.0, 650.0, 600.0, 550.0, 500.0, 450.0, 400.0, 350.0, 300.0, 250.0, 225.0, 200.0, 175.0, 150.0, 125.0, 100.0, 70.0]
+z_levs=np.array(z_levs)
+z_levs=100*z_levs
+number_of_zlevs  =len(z_levs)
+
--- writenc.py
+++ writenc.py
@@ -1,5 +1,5 @@
 from netCDF4 import Dataset
-def createsmnc(casename,vname,periods,nx,ny,dimension=3):
+def createsmnc(casename,vname,periods,fields,nx,ny,nz=None):
   filename            = "%s_%s_%s.nc"%(casename,vname,periods)
   rootgrp             = Dataset(filename                   , "w")
   rootgrp.createDimension("lat"      , nx )
@@ -9,26 +9,35 @@ def createsmnc(casename,vname,periods,nx,ny,dimension=3):
   subtime              = rootgrp.createVariable(periods    , "i4"  , (periods , ))
   subtime[:]           = range(1,nt+1)
   subtime.units        = periods
-  if dimension==3:
-    rootgrp.createDimension("time"     , None)
-    times               = rootgrp.createVariable("time"      , "i4"  , ("time"   , ))
-    times.units         = "year"
-    nc_var              = rootgrp.createVariable(vname,"f4",("time",periods,"lat","lon",))
+  rootgrp.createDimension("time"     , None)
+  times               = rootgrp.createVariable("time"      , "i4"  , ("time"   , ))
+  times.units         = "year"
+  if nz:
+    nc_dim=("time",periods,"bottom_top","lat","lon",)
+    rootgrp.createDimension("bottom_top"      , nz )
   else:
-    nc_var              = rootgrp.createVariable(vname,"f4",(periods,"lat","lon",))
+    nc_dim=("time",periods,"lat","lon",)
+  for field in fields:
+    rootgrp.createVariable(field,"f4",nc_dim)
   return rootgrp
 
 
-def createnc(casename,vname,periods,units,calendar,nx,ny,dimension=3):
+def createnc(casename,vname,periods,units,calendar,fields,nx,ny,nz=None):
   filename            = "%s_%s_%s.nc"%(casename,vname,periods)
   rootgrp             = Dataset(filename                   , "w")
-  rootgrp.createDimension("lat"      , nx )
-  rootgrp.createDimension("lon"      , ny )
+  rootgrp.createDimension("west_east"        , nx )
+  rootgrp.createDimension("south_north"      , ny )
   rootgrp.createDimension("time"     , None)
   T                   = rootgrp.createVariable("time"    , "f4"  , ("time" , ))
   T.units             = units
   T.calendar          = calendar
-  nc_var              = rootgrp.createVariable(vname,"f4",("time","lat","lon",))
+  if nz:
+    rootgrp.createDimension("bottom_top"      , nz )
+    nc_dim=("time","bottom_top","south_north","west_east",)
+  else:
+    nc_dim=("time","south_north","west_east",)
+  for field in fields:
+    rootgrp.createVariable(field,"f4",nc_dim)
   return rootgrp 
 
 
