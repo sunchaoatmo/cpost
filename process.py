006507b505c3f39de71eb0785f0aebca464b3103
Chao Sun <sunchao@umd.edu>
2017-04-10 17:53:54 -0400

    fix bug in the end of month defination change vname into taskname

--- process.py
+++ process.py
@@ -3,6 +3,7 @@ import numpy as np
 def anal_daily(iday,outputdata,wrf_o,wrf_i,taskname,fields,vert_intp,outputdim,z_levs,number_of_zlevs):
   from constant import RCP,p0
   from ARWpost import arwpost
+  import time
   pb   =wrf_i.variables['PB'][0,:,:,:]
   ntime       =wrf_o.dimensions['Time'].size
   south_north =wrf_o.dimensions['south_north'].size
@@ -21,31 +22,30 @@ def anal_daily(iday,outputdata,wrf_o,wrf_i,taskname,fields,vert_intp,outputdim,z
     psfc =None
     nz,ny,nx=bottom_top,south_north, west_east
     for itime in range(ntime):
-      if taskname=="conv":
-        qv   =wrf_o.variables['QVAPOR'][itime,:,:,:]
+      p    =wrf_o.variables['P'][itime,:,:,:]
+      pres =p+pb
       if taskname=="geopt" or taskname=="height" or taskname=="temp" :
         ph   =wrf_o.variables['PH'][itime,:,:,:]
         psfc =wrf_o.variables['PSFC'][itime,:,:]
         qv   =wrf_o.variables['QVAPOR'][itime,:,:,:]
         geopt_w=ph+phb
         geopt=(geopt_w[1:,:,:]+geopt_w[:-1,:,:])/2.0
-      p    =wrf_o.variables['P'][itime,:,:,:]
-      pres =p+pb
+        t  =wrf_o.variables['T'][itime,:,:,:]
+        tk = (t+300.) * ( pres / p0 )**RCP
+        theta = (t+300.)
 
       for field in fields:
         if field in wrf_o.variables:
           metfield=wrf_o.variables[field][itime,:,:,:]
           (ntime,nz,ny,nx)=wrf_o.variables[field].shape
         elif field=="tk":
-          t  =wrf_o.variables['T'][itime,:,:,:]
-          tk = (t+300.) * ( pres / p0 )**RCP
           metfield=tk
         elif field=="theta":
-          t  =wrf_o.variables['T'][itime,:,:,:]
-          theta = (t+300.)
           metfield=theta
-        elif field=="geopt":
+        elif field=="geopt" :
           metfield=geopt
+        elif field=="height":
+          metfield=geopt/9.8
         elif field=="u_met":
           metfield=wrf_o.variables["U"][itime,:,:,:]
           (ntime,nz,ny,nx)=wrf_o.variables["U"].shape
@@ -54,14 +54,13 @@ def anal_daily(iday,outputdata,wrf_o,wrf_i,taskname,fields,vert_intp,outputdim,z
           (ntime,nz,ny,nx)=wrf_o.variables["V"].shape
 
         """
-        elif taskname=="theta":
-        elif taskname=="cape":
         elif taskname=="td":
         elif taskname=="cldfr":
-        elif taskname=="cin":
         elif taskname=="dbz":
         """
+#       outputdata[field][iday,:,:,:]+= arwpost.interp(
         outputdata[field][iday,:,:,:]+= arwpost.interp(
+#             data_out=temp,
              cname=field                     , vertical_type=vert_intp         , 
              data_in=metfield                , 
              z_data=pres                     , 
@@ -70,7 +69,6 @@ def anal_daily(iday,outputdata,wrf_o,wrf_i,taskname,fields,vert_intp,outputdim,z
              geopt=geopt                     , tk=tk                           , qv=qv                        , 
              nx =nx                          , ny =ny                          , nz=nz                        , 
              bottom_top_dim=bottom_top       , south_north_dim=south_north     , west_east_dim=west_east)
-
     for field in fields:
       outputdata[field][iday,:,:,:]=outputdata[field][iday,:,:,:]/ntime
     if taskname=="uv_met":
@@ -105,10 +103,10 @@ def anal_daily(iday,outputdata,wrf_o,wrf_i,taskname,fields,vert_intp,outputdim,z
         outputdata[field][iday,:,:,:]=np.mean(metfield,axis=0)
       else:
         print("can only output to 3 or 4 dim")
+
   return 
 
-def anal_sea_mon(periods,rawnc,monthList,start_ymd,end_ymd,fields,vname,casename,shiftday,calendar_cur,time_units,units,description,nx,ny):
-  from default import r95t_hist
+def anal_sea_mon(periods,rawnc,monthList,start_ymd,end_ymd,fields,taskname,casename,shiftday,calendar_cur,time_units,units,description,nx,ny,nz,r95t_hist):
   import numpy.ma as ma
   from cs_stat import cs_stat
   from netCDF4 import date2num,num2date
@@ -116,16 +114,17 @@ def anal_sea_mon(periods,rawnc,monthList,start_ymd,end_ymd,fields,vname,casename
   import os.path
   from netCDF4 import Dataset
   from datetime import datetime,timedelta
-  from constant import *   # only several constants
+  from constant import seasonList,monthlyList,dry_lim,qvalue # only several constants
+  import time
   from writenc import createsmnc
   dodiag=False
   Oneday=timedelta(days=1)
 
   if datetime(start_ymd.year,12,1)+shiftday*Oneday>=start_ymd and datetime(end_ymd.year,11,1)<=end_ymd and start_ymd.year<end_ymd.year: 
-    diagfname="%s_%s_%s.nc"%(casename,vname,periods)
+    diagfname="%s_%s_%s.nc"%(casename,taskname,periods)
     diagexist=os.path.isfile(diagfname) 
     if diagexist:
-      diagfname="%s_%s_%s.nc"%(casename,vname,periods)
+      diagfname="%s_%s_%s.nc"%(casename,taskname,periods)
       diagnc=Dataset(diagfname,'a')
       nctime_diag=diagnc.variables["time"]
       lastindex=len(nctime_diag)
@@ -139,7 +138,10 @@ def anal_sea_mon(periods,rawnc,monthList,start_ymd,end_ymd,fields,vname,casename
       dodiag=True
       diag_startyear=start_ymd.year+1
       diag_endyear=end_ymd.year
-      diagnc=createsmnc(casename,postvar,periods,fields,nx,ny)
+      diagnc=createsmnc(casename,taskname,periods,fields,nx,ny,nz)
+      for field in fields:
+        diagnc.variables[field].units=units[field]
+        diagnc.variables[field].description=description[field]
 
   if dodiag:
     Years=range(diag_startyear,diag_endyear+1)
@@ -147,7 +149,7 @@ def anal_sea_mon(periods,rawnc,monthList,start_ymd,end_ymd,fields,vname,casename
     for i,year in  enumerate(Years):
       i_cur=i+lastindex
       for j,month in enumerate(monthList):
-        if 12 in month:
+        if 12 == month[0]:
           byear=year-1
         else:
           byear=year
@@ -161,14 +163,14 @@ def anal_sea_mon(periods,rawnc,monthList,start_ymd,end_ymd,fields,vname,casename
           eday=calendar.monthrange(eyear, emonth)[1]
         ymd_datetime=datetime(int(byear),int(bmonth),1,0,0,0)
         dayb=date2num(ymd_datetime,units=time_units,calendar=calendar_cur)
-        print(ymd_datetime)
+        print("%s analysis beg:%s"%(periods,ymd_datetime))
         ymd_datetime=datetime(int(eyear),int(emonth),int(eday),0,0,0)
-        print(ymd_datetime)
+        print("%s analysis end:%s"%(periods,ymd_datetime))
         daye=date2num(ymd_datetime,units=time_units,calendar=calendar_cur)
         dayb=dayb-rawnc.variables["time"][0]+shiftday
         daye=daye-rawnc.variables["time"][0]+shiftday
-        data_daily_ma=ma.masked_values(rawnc.variables[vname][int(dayb):int(daye),:,:],1.e+20)
-        if vname=="PRAVG":
+        if taskname=="PR":
+          data_daily_ma=ma.masked_values(rawnc.variables["PRAVG"][int(dayb):int(daye),:,:],1.e+20)
           if periods=="seasonal":
             R95T_HIST=r95t_hist.variables["R95T_hist"][j]
           else:
@@ -178,12 +180,16 @@ def anal_sea_mon(periods,rawnc,monthList,start_ymd,end_ymd,fields,vname,casename
            diagnc.variables["R5D"][i_cur,j,:,:],
            diagnc.variables["SDII"][i_cur,j,:,:],
            diagnc.variables["R95T"][i_cur,j,:,:])=cs_stat.precp_extrem(fields=data_daily_ma,r95t_hist=R95T_HIST,dry_lim=dry_lim)
-          diagnc.variables["PCT"][i_cur,j,:,:]=cs_stat.quantile_cal(data_daily_ma,dry_lim,pct)
+          diagnc.variables["PCT"][i_cur,j,:,:]=cs_stat.quantile_cal(pre_quantile=data_daily_ma,dry_lim=dry_lim,qvalue=qvalue)
           diagnc.variables["CDD"][i_cur,j,:,:]=cs_stat.consective_dry(fields=data_daily_ma,dry_lim=dry_lim)
-        diagnc.variables[vname].units=units
-        diagnc.variables[vname].description=description
-        diagnc.variables[vname][i_cur,j,:,:]=np.mean(data_daily_ma,axis=0)
+          diagnc.variables["PRAVG"][i_cur,j,:,:]=np.mean(data_daily_ma,axis=0)
+        else:
+          for field in fields:
+            data_daily_ma=ma.masked_values(rawnc.variables[field][int(dayb):int(daye),:,:],1.e+20)
+            diagnc.variables[field][i_cur,j,:,:]=np.mean(data_daily_ma,axis=0)
         print("year %s season %s"% (str(year),str(j)))
+    diagnc.history ='Created by Chao Sun sunchao@umd.edu ' + time.ctime(time.time())
+    diagnc.source ='CWRF run:%s'%casename
     diagnc.close()
 
 def wrftimetodate(wrfstr):
